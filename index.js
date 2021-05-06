dacdc608d97e3691c5d7a84ac7890486be989f81
pseno <pasalicseno@hotmail.com>
2021-05-06 14:51:25 +0200

    Add files

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const fetch = require('node-fetch');
+const fs = require('fs');
 const cheerio = require('cheerio');
 
-fetch('https://memegen-link-examples-upleveled.netlify.app/')
-  .then((res) => res.text())
-  .then((body) => {
-    console.log(body);
+const siteUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
+
+fs.mkdirSync('./Meme', { recursive: true });
+console.log("Finding or creating 'Meme' folder...");
+
+const getMemes = async () => {
+  const response = await fetch(siteUrl);
+  const body = await response.text();
+
+  const $ = cheerio.load(body);
+  const linkList = [];
+
+  $('img').each((i, link) => {
+    if (i < 10) {
+      linkList.push(link.attribs.src);
+    }
   });
+
+  for (let j = 0; j < 10; j++) {
+    const url = linkList[j];
+
+    async function download() {
+      const response2 = await fetch(url);
+      const buffer = await response2.buffer();
+      fs.writeFile(`./Meme/image${j}.jpg`, buffer, () =>
+        console.log('finished downloading!'),
+      );
+    }
+    download(linkList[j]);
+  }
+};
+
+getMemes();
