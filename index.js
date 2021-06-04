94c0f401b985e29d99980dc9e736f653fa3a6f62
pseno <pasalicseno@hotmail.com>
2021-06-04 16:59:11 +0200

    Change file

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const fetch = require('node-fetch');
-const fs = require('fs');
-const cheerio = require('cheerio');
+const fetch = require('node-fetch'); // this is a library
+const fs = require('node:fs');
+const cheerio = require('cheerio'); // this is a library
 
 const siteUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
 
-fs.mkdirSync('./Meme', { recursive: true });
+fs.mkdirSync('./meme', { recursive: true });
 console.log("Finding or creating 'Meme' folder...");
 
 const getMemes = async () => {
@@ -19,7 +19,7 @@ const getMemes = async () => {
       linkList.push(link.attribs.src);
     }
   });
-
+  console.log(linkList);
   for (let j = 0; j < 10; j++) {
     const url = linkList[j];
 
